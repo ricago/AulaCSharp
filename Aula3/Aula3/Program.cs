cf9a95ee8851049b1879f81d7c2f8a88d8b8c349
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-10-13 17:01:29 +0100

    Aula 3 com exercicios

---
+++ Aula3/Aula3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo-----------------------------------------------------------------------
+
+            //string nome = "João";
+            //string tempoAtual = "Chuva";
+            //string temperaturaAtual = "18º";
+
+            //Console.WriteLine("Ola {0}. Hoje o dia está de {1}. Estão {2}", "João", "Chuva", "18º");
+            //Console.WriteLine("Ola {0}. Hoje o dia está de {1}. Estão {2}",nome,tempoAtual,temperaturaAtual);
+
+            //Console.WriteLine($"Ola {nome}. Hoje o dia está de {tempoAtual}. Estão {temperaturaAtual}");
+            //---------------------------------------------------------------------------------------------------------
+            //Console.WriteLine("Indique o numero para calcular o cubo");
+            //int numero = Convert.ToInt32(Console.ReadLine());
+            //double resultado = CalculaCubo(numero);
+            //Console.WriteLine(resultado);
+
+            //static double CalculaCubo(int numeroCalcular)
+            //{
+            //    return Math.Pow(numeroCalcular, 3);
+
+            //}
+
+            //----------------------------------------------------------------------------------------------
+
+            //            if(args.Length > 1)
+            //{
+            //                throw new ArgumentException("Tem um argumento a mais");
+            //}
+            //----------------------------------------------------------------------------------------------------
+
+            try
+            {
+                if (args.Length > 1)
+                {
+                    throw new ArgumentException("Tem um argumento a mais"); ;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um erro");
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                Console.WriteLine(Inner);
+            }
+            finally
+            {
+                ;
+            }
+
+
+        }
+    }
+}
---
+++ Exercicios aula 3/Exercicios aula 3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Exercicios_aula_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Digite dois valor:");
+            //try
+            //{
+            //    int n1 = int.Parse(Console.ReadLine());
+            //    int n2 = int.Parse(Console.ReadLine());
+
+            //    if (n1 == n2)
+            //    {
+            //        Console.WriteLine("Os dois valores são iguais.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Os dois valor são diferentes");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Digite apenas numeros.");
+
+            //}
+            //finally
+            //{
+            //Console.WriteLine();
+            //Console.WriteLine("O Programa correu até ao fim");
+            //}
+            //outro exercicio-------------------------------------------------------------------
+
+            //Console.WriteLine("Digite a sua idade:");
+            //try
+            //{
+            //    int idade = int.Parse(Console.ReadLine());
+
+            //    if (idade >= 18)
+            //    {
+            //        Console.WriteLine("Pode votar.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Não pode votar.");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Digite apenas a sua idade.");
+
+            //}
+            //finally
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("O Programa correu até ao fim");
+            //}
+
+            //outro exerc.-------------------------------------------------------------------------------
+
+            Console.WriteLine("Digite o numero correspodente ao dia da semana.");
+            Console.WriteLine("1 - Segunda");
+            Console.WriteLine("2 - Terça");
+            Console.WriteLine("3 - Quarta");
+            Console.WriteLine("4 - Quinta");
+            Console.WriteLine("5 - Sexta");
+            Console.WriteLine("6 - Sábado");
+            Console.WriteLine("7 - Domingo");
+            try { 
+            int dia = int.Parse(Console.ReadLine());
+
+            switch (dia)
+            {
+                case 1: 
+                    Console.WriteLine("Segundo-feira :(");
+                    break;
+                case 2:
+                    Console.WriteLine("Terça-feira :(");
+                    break;
+                case 3:
+                    Console.WriteLine("Quarta-feira :|");
+                    break;
+                case 4:
+                    Console.WriteLine("Quinta-feira :)");
+                    break;
+                case 5:
+                    Console.WriteLine("Sexta-feira :D");
+                    break;
+                case 6:
+                    Console.WriteLine("Sabado :D");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo :(");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Existem apenas 7 dias da semana");
+                    break;
+            }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Digite apenas numeros de 1 a 7");
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("O programa correu até ao fim");
+            }
+        }    
+    }
+}
