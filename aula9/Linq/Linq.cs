e2b70d994c824261f9ecb1383c646d3d781e4d48
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-11-17 16:54:35 +0000

    aula8- Dicionario e LINQ

---
+++ aula9/Linq/Linq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    internal class Linq
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new() {55,-2,0,19,43,25,5,99};
+
+            var desce = num.OrderByDescending(x => x).ToList();
+
+            var maiores = desce.Take(5);
+
+            foreach (var numero in maiores)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
---
+++ aula9/aula9/Dicionario.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace aula9
+{
+    internal class Dicionario
+    {
+        static void Main(string[] args)
+        {
+            int op, num;
+            string nome;
+
+            try
+            {
+                Dictionary<string, int> ListaAlunos = new Dictionary<string, int>();
+                do
+                {
+
+                    Console.WriteLine("1 - Adicionar aluno");
+                    Console.WriteLine("2 - Remover aluno");
+                    Console.WriteLine("0 - Sair");
+
+                    Console.WriteLine("Selecione a sua operação");
+                    op = int.Parse(Console.ReadLine());
+
+                    switch (op)
+                    {
+                        case 1:
+                            Console.Clear();
+
+                            Console.WriteLine("Digite o numero de alunos que deseja adicionar: ");
+                            int nAlunos = int.Parse(Console.ReadLine());
+
+                            for(int i = 0; i < nAlunos; i++) { 
+                            Console.WriteLine("Digite o nome do aluno");
+                            nome = Console.ReadLine();
+                            do
+                            {
+                                Console.WriteLine("Digite o numero do aluno");
+                                num = int.Parse(Console.ReadLine());
+                            } while (num < 0);
+                            ListaAlunos.Add(nome, num);
+                            Console.WriteLine("O aluno {0} foi adicionado com o numero {1}.", nome,num);
+                            }
+                            Console.WriteLine("Pressione Enter para continuar");
+                            string lixo = Console.ReadLine();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Digite o aluno que deseja remover: ");
+                            string remover = Console.ReadLine();
+
+                            if (ListaAlunos.ContainsKey(remover))
+                            {
+                                ListaAlunos.Remove(remover);
+                                Console.WriteLine("O aluno {0} foi removido.", remover);
+                            }
+                            else
+                            {
+                                Console.WriteLine("O aluno {0} não existe.", remover);
+                            }
+                            break;
+                    }
+                } while (op != 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um erro");
+            }
+        }
+    }
+}
